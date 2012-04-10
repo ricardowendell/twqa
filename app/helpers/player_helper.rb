85f8aefe2d6ca315fde73cf1cb48bddda1647be3
Jarrod Sibbison <jsibbiso@JSibbisons15MBP1AGZ.local>
2012-04-10 10:21:55 +1000

    Add extra cities for registration : Jarrod

---
+++ app/helpers/player_helper.rb
@@ -0,0 +1,9 @@
+module PlayerHelper
+  def self.cities
+    ['Melbourne','Adelaide','Brisbane','Canberra','Darwin','Hobart','Perth','Sydney','Other']
+  end
+  
+  def self.roles
+    ['CIO/CTO','Consultant','CXO Other','Developer','Director','Manager','Technologist','VP','Other']
+  end
+end
