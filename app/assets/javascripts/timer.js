c4c4e52cece800f97acb99e6aa3d9cc440eaf07c
Jarrod Sibbison <jsibbiso@JSibbisons15MBP1AGZ.local>
2012-04-11 16:27:08 +1000

    refactor timer to clean up : Jarrod

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,18 +1,13 @@
 function Timer() {
-	// this.it = 1;
-	// 	this.get_it = function() {
-	// 		return this.it;
-	// 	};
-	
-	this.start_time = 0;
-	this.end_time = 0;
-	this.astart = function() {
-		this.start_time = new Date().getTime();
+	this.startTime = 0;
+	this.endTime = 0;
+	this.start = function() {
+		this.startTime = new Date().getTime();
 	};
-	this.astop = function() {
-		this.end_time = new Date().getTime();
+	this.stop = function() {
+		this.endTime = new Date().getTime();
 	};
 	this.elapsed = function() {
-		return this.end_time - this.start_time;
+		return this.endTime - this.startTime;
 	};
 }
