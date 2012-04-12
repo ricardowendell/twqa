c1f695c4eb31c915cb44587909e7bffa5232b7ac
Jarrod Sibbison <jsibbiso@JSibbisons15MBP1AGZ.local>
2012-04-12 17:10:24 +1000

    Make player email unique : Jarrod

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 class Player < ActiveRecord::Base
   validates :first_name, :last_name, :email, :mobile_number, :city, :company_name, :role, :presence => {:message => 'required'}
-  validates :email, :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i , :message => 'invalid email'}, :presence => {:message => 'required'}
+  validates :email, :uniqueness => {:message => 'email already registered'}, :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i , :message => 'invalid email'}, :presence => {:message => 'required'}
   
   def self.csv_data
     CSV.generate do |csv|
