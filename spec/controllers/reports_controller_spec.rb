22903ba671a68c46aa1c3cc6843bd5513e5c6995
yongpeishi <pjong@thoughtworks.com>
2012-04-12 22:30:23 +1000

    ps: more validation on csv report

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -1,22 +1,36 @@
 require 'spec_helper'
 
 describe ReportsController do
-  before(:each) do
+  let(:login) {
     @request.env['HTTP_AUTHORIZATION'] =
       ActionController::HttpAuthentication::Basic.encode_credentials('twqaadmin','password')
-  end
+  }
 
   describe "GET 'index'" do
-    it "returns http success" do
+    it "returns http failure before login" do
+      get 'index'
+      response.should_not be_success
+    end
+
+    it "returns http success after login" do
+      login
       get 'index'
       response.should be_success
     end
   end
 
   describe "GET 'download'" do
-    it "returns http success" do
+    it "returns http failure before login" do
+      get 'download'
+      response.should_not be_success
+    end
+
+    it "returns http success after login" do
+      login
       get 'download'
       response.should be_success
+      response.headers['Content-Disposition'].should include 'filename="player_data.csv"'
+      response.headers['Content-Type'].should include 'text/csv'
     end
   end
 
