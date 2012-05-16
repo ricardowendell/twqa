245bf3121e0bf5ddfcc75df3582c2609abc1b1cf
yongpeishi <pjong@thoughtworks.com>
2012-05-16 10:48:45 +1000

    ps: style questions page

--- app/assets/javascripts/qa.js
+++ app/assets/javascripts/qa.js
@@ -20,32 +20,37 @@ $(document).ready(function() {
   function renderQuestions(data, num) {
     $("#questions .question").remove();
     var toRender = $.shuffle(data).slice(0, num);
+
     $.each(toRender, function(index, item) {
 
       var template =
               "<div class='question'>" +
                       "<div class='question_id'>{{question_id}}</div>" +
-                      "<h2 class='question_text'>{{question}}</h2>" +
+                      "<div class='question_text'>Q" + (index+1) + ": {{question}}</div>" +
                       "<div class='options'>" +
                       "<ol class='choices'>" +
                       "{{#choices}}" +
-                      "<li>" +
-                      "<a href='#' class='btn large primary'>{{.}}</a>" +
+                      "<li class='choice'>" +
+                      "<a href='#' class='btn answer'><span>{{.}}</span></a>" +
                       "</li>" +
                       "{{/choices}}" +
                       "</ol>" +
                       "</div>" +
-                      "<div class='answer'>{{correct_choice}}</div>" +
+                      "<div class='correct_answer'>{{correct_choice}}</div>" +
                       "</div>";
 
       var question = Mustache.to_html(template, item);
       $("#questions").append(question);
+      $('li.choice').each(function(index) {
+        $(this).find('a').addClass("btn answer" + (index+1)%2 );
+      });
     });
   }
 
   function play() {
     $("#win").hide();
     $("#lose").hide();
+	$("#alert-actions").hide();
     $("#incorrect_questions .incorrect_question").remove();
     attemptedQuestionsLog = [];
  	$('#clock').show();
@@ -65,7 +70,7 @@ $(document).ready(function() {
 
     question.on("click", ".choices a", function(event) {
       var chosenAnswer = $(this).text();
-      var correctAnswer = question.find(".answer").text();
+      var correctAnswer = question.find(".correct_answer").text();
 
       if (chosenAnswer === correctAnswer) {
         score += 1;
@@ -87,21 +92,23 @@ $(document).ready(function() {
 	  $.post('/timers/'+$('#player_id').text()+'/record',{time_seconds:timer.elapsedSeconds()})
 		.success(function() {
 			$.get('/leaderboard/player_position/'+$('#player_id').text(), function(data) {
-				$('#leaderboard-position').text('Leaderboard position is #' + data)
+				$('#leaderboard-position').text('Your leaderboard position is #' + data)
 			});
 		});
       $("#win").toggle();
     } else {
       $("#lose").toggle();
+      $('#clock').hide();
     }
+	$("#alert-actions").toggle();
   }
 
   function recordIncorrectQuestion(question) {
       var questionText = question.find(".question_text").text();
-      var answer = question.find(".answer").text();
+      var answer = question.find(".correct_answer").text();
       var template =
         "<div class='incorrect_question'>" +
-        "<h3>" + questionText + "</h3>" +
+        "<p>" + questionText + "</p>" +
         "<div>Answer: " + answer + "</div>" +
         "</div>";
 
@@ -125,7 +132,7 @@ $(document).ready(function() {
 	  $.post('/attempted_questions/'+$('#player_id').text()+'/record', {attempted_questions:attemptedQuestionsLog});
   }
 
-  $(".alert-actions a:nth-child(1)").on("click", function(event) {
+  $("#alert-actions a:nth-child(1)").on("click", function(event) {
     play();
   });
 
