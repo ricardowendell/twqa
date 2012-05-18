e776deaf95a6634f6609d3aa9774be8fac5cd68b
yongpeishi <pjong@thoughtworks.com>
2012-05-18 10:12:49 +1000

    ps: added leaderboard layout

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -2,9 +2,9 @@ class LeaderboardsController < ApplicationController
   def players
     @players = Player.joins(:timer).includes(:timer).find(:all, :limit => 10,
                                                           :order => "time_seconds ASC")
-    render :layout => nil
+    render :layout => 'leaderboard'
   end
-  
+
   def player_position
     player_id = params[:player_id].to_i
     position = Timer.find_by_sql("select count(*) as position from timers where time_seconds < (select time_seconds from timers where player_id = #{player_id})").first.position.to_i + 1
