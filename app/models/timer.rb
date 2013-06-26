0798ccbd137dd665922da65a11c151d10a773fba
Rafael Portela <rafaelportela90@gmail.com>
2013-06-26 18:38:34 -0300

    Revert "Merge branch 'master' of heroku.com:trivia-app"
    This reverts commit a68f0c4de74ff53f0d323f55ffbe5e3373bd41ff, reversing
    changes made to 8faa8120a19988dc15a3583cc850a44dfff660cf.

--- app/models/timer.rb
+++ app/models/timer.rb
@@ -1,9 +1,6 @@
 class Timer < ActiveRecord::Base
   belongs_to :player
   set_primary_key "player_id"
-  
-  validates_numericality_of :time_seconds, :greater_than => 0
-  
 
   def self.attributes_protected_by_default
     ["created_at", "updated_at"]
--- spec/models/timer_spec.rb
+++ spec/models/timer_spec.rb
@@ -1,11 +1,4 @@
 require 'spec_helper'
 
 describe Timer do
-  
-  it "should not accept negative time" do
-    invalid_timer = Timer.new :time_seconds => -1
-    invalid_timer.should_not be_valid
-  end
-  
-  
 end
